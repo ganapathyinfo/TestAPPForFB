7ebd2c281dcdf2d616f39af1e65094224ce26c50
ganapathyinfo <ganapathy.p@tychons.com>
2014-08-05 20:25:57 +0530

    Testapp
    Testapp for Facbook

---
+++ iHelp+/gen/com/ihelpplusapps/ihelpplus/socialmedia/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.ihelpplusapps.ihelpplus.socialmedia;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
